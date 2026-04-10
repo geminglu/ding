7a19777b42ecd0b78eee89bff84c99457d20a2a8
minglu_ge <geminglu199698@outlook.com>
2026-04-10 21:52:57 +0800

    feat(app): update dependencies and restructure app layout

---
+++ apps/app/app/index.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+import { ThemedView } from "@/components/themed-view";
+
+export default function HomeScreen() {
+  return <ThemedView></ThemedView>;
+}
+
+const styles = StyleSheet.create({});
